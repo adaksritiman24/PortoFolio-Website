51a6e95c579a2ba3020ad63ef7bea59247bbf9a3
Sritiman Adak <adaksritiman24@gmail.com>
2022-03-10 09:56:26 +0530

    heroku configurations

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vlh5h0&d2i(9h!48t&i@havi6-f5i03v-(y^nps-do#s%7&3(k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sritiman-adak.herokuapp.com','localhost']
 
 
 # Application definition
